52e546464c2bc18c9a3f34065666a80a490c7eab
Thiago Guedes <guedes86@gmail.com>
2024-08-21 20:24:43 -0500

    ejercicio tabla 21-08

---
+++ script.js
@@ -0,0 +1,14 @@
+function mult(){
+    let number = document.getElementById('number').value;
+
+    document.getElementById('mult1').textContent = (number+" x 1 = " + number*1 );
+    document.getElementById('mult2').textContent = (number+" x 2 = " + number*2 );
+    document.getElementById('mult3').textContent = (number+" x 3 = " + number*3 );
+    document.getElementById('mult4').textContent = (number+" x 4 = " + number*4 );
+    document.getElementById('mult5').textContent = (number+" x 5 = " + number*5 );
+    document.getElementById('mult6').textContent = (number+" x 6 = " + number*6 );
+    document.getElementById('mult7').textContent = (number+" x 7 = " + number*7 );
+    document.getElementById('mult8').textContent = (number+" x 8 = " + number*8 );
+    document.getElementById('mult9').textContent = (number+" x 9 = " + number*9 );
+    document.getElementById('mult10').textContent = (number+" x 10 = " + number*10 );
+}
